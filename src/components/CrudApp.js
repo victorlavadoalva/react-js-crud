a6a9e3bb1ec16cd8e249bcc9cec537f12062ab3a
victor <victor.lavado15@hotmail.com>
2022-12-02 12:34:07 -0500

    edicion de datos y comunic de component lista

--- src/components/CrudApp.js
+++ src/components/CrudApp.js
@@ -41,7 +41,10 @@ const CrudApp = () => {
     setDb([...db, data]);
   };
 
-  const updateData = (data) => {};
+  const updateData = (data) => {
+    let newData = db.map(el => el.id === data.id ? data : el);
+    setDb(newData);
+  };
 
   const deleteData = (id) => {};
 
--- src/components/CrudForm.js
+++ src/components/CrudForm.js
@@ -10,6 +10,14 @@ const CrudForm = ({ createData, updateData, dataToEdit, setDataToEdit }) => {
  
   const [form, setForm] = useState(initialForm);
 
+  useEffect(()=>{
+    if(dataToEdit){
+      setForm(dataToEdit);
+    }else {
+      setForm(initialForm);
+    }
+  }, [dataToEdit])
+
   const handleChange = (e) => {
     setForm({
         ...form,
@@ -41,7 +49,7 @@ const CrudForm = ({ createData, updateData, dataToEdit, setDataToEdit }) => {
 
   return (
     <div>
-      <h3>Agregar</h3>
+      <h3>{dataToEdit ? "Editar": "Agregar"}</h3>
       <form onSubmit={handleSubmit}>
         <input
           type="text"
